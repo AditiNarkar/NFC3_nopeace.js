89ab41049edd52683f2eae1927c55def80c5955b
Tufayl18 <tufayldalvi007@gmail.com>
2024-08-29 22:08:54 +0530

    wallet

--- web3_medical_resources_frontend/app/page.js
+++ web3_medical_resources_frontend/app/page.js
@@ -1,30 +1,41 @@
+
 import styles from "./styles.module.css";
-import Image from 'next/image'
-import paper from "../assets/paperPhoto.png"
+import Image from "next/image";
+import paper from "../assets/paperPhoto.png";
 export default function Home() {
+
   return (
     <>
-      <div style={{ display: "flex", gap: 20, padding: "10px 20px", }}>
+      <div style={{ display: "flex", gap: 20, padding: "10px 20px" }}>
         <div>
-          <input style={{ width: 600, height: 30 }} placeholder="Search"></input>
+          <input
+            style={{ width: 600, height: 30 }}
+            placeholder="Search"
+          ></input>
         </div>
-        <button style={{ left: "36vw" }} className={styles.ConnectButton}>Upload</button>
-
+        <button style={{ left: "36vw" }} className={styles.ConnectButton}>
+          Upload
+        </button>
       </div>
 
       <div className={styles.pageContainer}>
         <div className={styles.paperDetails}>
-          <Image
-            src={paper}
-            width={310}
-            height={200}
-          ></Image>
-          <div style={{ display: "flex", justifyContent: "center", fontWeight: 600 }}>Title</div>
+          <Image src={paper} width={310} height={200}></Image>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              fontWeight: 600,
+            }}
+          >
+            Title
+          </div>
           <div>Posted By:</div>
 
-          <button style={{ width: "100%" }} className={styles.ConnectButton}>View</button>
+          <button style={{ width: "100%" }} className={styles.ConnectButton}>
+            View
+          </button>
         </div>
-
       </div>
     </>
   );
--- web3_medical_resources_frontend/components/header.js
+++ web3_medical_resources_frontend/components/header.js
@@ -1,27 +1,34 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 import styles from "../app/styles.module.css";
-import Image from 'next/image'
+import Image from "next/image";
 import logo from "../assets/medicalchain_logo.png";
 
 export default function Header() {
-    return (
-        <>
-            <div className={styles.Header}>
-                <div className={styles.HeaderChild1}>
-
-                    <Image
-                        src={logo}
-                        width={100}
-                        height={70}
-                    />
-                    <div>About</div>
-                    <div>Contact</div>
-
-                </div>
-                <div className={styles.HeaderChild2}>
-                    <span > wallet address</span>
-                    <button className={styles.ConnectButton}>Connect</button>
-                </div>
-            </div >
-        </>
-    );
+  const { ready, authenticated, login, logout } = usePrivy();
+  const { wallets } = useWallets();
+  return (
+    <>
+      <div className={styles.Header}>
+        <div className={styles.HeaderChild1}>
+          <Image src={logo} width={100} height={70} />
+          <div>About</div>
+          <div>Contact</div>
+        </div>
+        <div className={styles.HeaderChild2}>
+          <span> wallet address</span>
+          {authenticated ? (
+            <button className={styles.ConnectButton} onClick={logout}>
+              Disconnect
+            </button>
+          ) : (
+            <button className={styles.ConnectButton} onClick={login}>
+              connect
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
